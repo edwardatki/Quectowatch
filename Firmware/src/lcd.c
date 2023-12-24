57075f1303b143c16cb281648ff589e9bcd979b4
Ed Atkinson <edwardatki@gmail.com>
2023-12-24 22:13:17 +0000

    Tidy up and notification text scrolling attempt

---
+++ Firmware/src/lcd.c
@@ -0,0 +1,276 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "esp_pm.h"
+#include "driver/spi_master.h"
+#include "driver/ledc.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_types.h"
+#include "lvgl.h"
+
+#define LCD_HOST  SPI2_HOST
+
+#define LCD_PIXEL_CLOCK_HZ (60 * 1000 * 1000)// (20 * 1000 * 1000)
+#define LCD_SCLK_PIN 2
+#define LCD_MOSI_PIN 3
+#define LCD_MISO_PIN 0
+#define LCD_DC_PIN 7
+#define LCD_RST_PIN 6
+#define LCD_CS_PIN 10
+#define LCD_BL_PIN 5
+
+#define LCD_H_RES 240
+#define LCD_V_RES 240
+
+#define LCD_CMD_BITS 8
+#define LCD_PARAM_BITS 8
+
+#define LEDC_TIMER LEDC_TIMER_0
+#define LEDC_MODE LEDC_LOW_SPEED_MODE
+#define LEDC_CHANNEL LEDC_CHANNEL_0
+#define LEDC_DUTY_RES LEDC_TIMER_13_BIT
+#define LEDC_DUTY 2048 // 25%
+#define LEDC_FREQUENCY 100
+
+#define LVGL_TICK_PERIOD_MS 2
+#define LVGL_TASK_MAX_DELAY_MS 100
+#define LVGL_TASK_MIN_DELAY_MS 1
+#define LVGL_TASK_STACK_SIZE 4096
+#define LVGL_TASK_PRIORITY 3
+
+static const char *_TAG = "LCD";
+
+lv_disp_t *disp;
+
+SemaphoreHandle_t lvgl_mux = NULL;
+
+int lvgl_task_running = 0;
+
+void anim_x_cb(void *var, int32_t v) {
+    lv_obj_set_x(var, v);
+}
+
+void anim_y_cb(void *var, int32_t v) {
+    lv_obj_set_y(var, v);
+}
+
+// Tell lvgl that disaply refresh is finished
+static bool notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx) {
+  lv_disp_drv_t *disp_driver = (lv_disp_drv_t *)user_ctx;
+  lv_disp_flush_ready(disp_driver);
+  return false;
+}
+
+// Copy buffer content to a specific area of the LCD
+static void lvgl_flush_cb(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_data) {
+  esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t) drv->user_data;
+  esp_lcd_panel_draw_bitmap(panel_handle, area->x1, area->y1, area->x2 + 1, area->y2 + 1, color_data);
+}
+
+// Called when driver parameters are updated, only used for changing rotation
+static void lvgl_port_update_callback(lv_disp_drv_t *drv) {
+  esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t) drv->user_data;
+
+  switch (drv->rotated) {
+    case LV_DISP_ROT_NONE:
+      // Rotate LCD
+      esp_lcd_panel_swap_xy(panel_handle, false);
+      esp_lcd_panel_mirror(panel_handle, true, false);
+      break;
+    case LV_DISP_ROT_90:
+      // Rotate LCD LCD
+      esp_lcd_panel_swap_xy(panel_handle, true);
+      esp_lcd_panel_mirror(panel_handle, true, true);
+      break;
+    case LV_DISP_ROT_180:
+      // Rotate LCD LCD
+      esp_lcd_panel_swap_xy(panel_handle, false);
+      esp_lcd_panel_mirror(panel_handle, false, true);
+      break;
+    case LV_DISP_ROT_270:
+      // Rotate LCD LCD
+      esp_lcd_panel_swap_xy(panel_handle, true);
+      esp_lcd_panel_mirror(panel_handle, false, false);
+      break;
+  }
+}
+
+// Update lvgl's internal tick system
+static void increase_lvgl_tick(void *arg) {
+  lv_tick_inc(LVGL_TICK_PERIOD_MS);
+}
+
+bool lvgl_lock(int timeout_ms) {
+  // If timeout_ms is -1 will block indefinetly waiting for mutex
+  const TickType_t timeout_ticks = (timeout_ms == -1) ? portMAX_DELAY : (timeout_ms / portTICK_PERIOD_MS);
+  ESP_LOGD(_TAG, "waiting for lvgl mutex");
+  return xSemaphoreTakeRecursive(lvgl_mux, timeout_ticks) == pdTRUE;
+}
+
+void lvgl_unlock() {
+  ESP_LOGD(_TAG, "releasing lvgl mutex");
+  xSemaphoreGiveRecursive(lvgl_mux);
+}
+
+// Main LVGL task
+static void lvgl_port_task(void *arg) {
+  ESP_LOGI(_TAG, "lvgl task start");
+  lvgl_task_running = 1;
+  static esp_pm_lock_handle_t pm_cpu_lock = NULL;
+  if (pm_cpu_lock == NULL) esp_pm_lock_create(ESP_PM_CPU_FREQ_MAX, 0, "lvgl_cpu", &pm_cpu_lock);
+  assert(pm_cpu_lock);
+
+  uint32_t task_delay_ms = LVGL_TASK_MAX_DELAY_MS;
+  while (1) {
+    // Lock the mutex due as LVGL APIs are not thread-safe
+    if (lvgl_lock(-1)) {
+      // Do update
+      esp_pm_lock_acquire(pm_cpu_lock); // Lets crank some frames... doesn't actually seem to make any difference
+      task_delay_ms = lv_timer_handler();
+      // Release the mutex
+      esp_pm_lock_release(pm_cpu_lock);
+      lvgl_unlock();
+    }
+    // Clamp delay time
+    if (task_delay_ms > LVGL_TASK_MAX_DELAY_MS) break;
+    else if (task_delay_ms < LVGL_TASK_MIN_DELAY_MS) task_delay_ms = LVGL_TASK_MIN_DELAY_MS;
+
+    // Delay as long as LVGL requested
+    ESP_LOGI(_TAG, "lvgl task waiting %lu", task_delay_ms);
+    vTaskDelay(task_delay_ms / portTICK_PERIOD_MS);
+  }
+
+  ESP_LOGI(_TAG, "lvgl task done");
+  esp_pm_lock_delete(pm_cpu_lock);
+  lvgl_task_running = 0;
+  vTaskDelete(NULL);
+}
+
+void lvgl_reactivate() {
+  // TODO query properly with freertos functions
+  if (!lvgl_task_running) {
+    ESP_LOGI(_TAG, "create lvgl task");
+    xTaskCreate(lvgl_port_task, "LVGL", LVGL_TASK_STACK_SIZE, NULL, LVGL_TASK_PRIORITY, NULL);
+  }
+}
+
+void init_lcd() {
+  static lv_disp_draw_buf_t disp_buf; // contains internal graphic buffer(s) called draw buffer(s)
+  static lv_disp_drv_t disp_drv;      // contains callback functions
+
+  ESP_LOGI(_TAG, "initialize backlight pwm");
+  gpio_sleep_sel_dis(LCD_BL_PIN);
+  // Prepare and then apply the LEDC PWM timer configuration
+  ledc_timer_config_t ledc_timer = {
+      .speed_mode       = LEDC_MODE,
+      .duty_resolution  = LEDC_DUTY_RES,
+      .timer_num        = LEDC_TIMER,
+      .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
+      .clk_cfg          = LEDC_USE_RC_FAST_CLK  // Use RTC clock in order to keep working in light sleep
+  };
+  ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+  // Prepare and then apply the LEDC PWM channel configuration
+  ledc_channel_config_t ledc_channel = {
+      .speed_mode     = LEDC_MODE,
+      .channel        = LEDC_CHANNEL,
+      .timer_sel      = LEDC_TIMER,
+      .intr_type      = LEDC_INTR_DISABLE,
+      .gpio_num       = LCD_BL_PIN,
+      .duty           = 0, // Set duty to 0%
+      .hpoint         = 0
+  };
+  ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+  
+
+  ESP_LOGI(_TAG, "initialize spi bus");
+  spi_bus_config_t buscfg = {
+      .sclk_io_num = LCD_SCLK_PIN,
+      .mosi_io_num = LCD_MOSI_PIN,
+      .miso_io_num = LCD_MISO_PIN,
+      .quadwp_io_num = -1,
+      .quadhd_io_num = -1,
+      .max_transfer_sz = LCD_H_RES * 80 * sizeof(uint16_t),
+  };
+  // Initialize LCD
+  ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
+
+  ESP_LOGI(_TAG, "install panel io");
+  esp_lcd_panel_io_handle_t io_handle = NULL;
+  esp_lcd_panel_io_spi_config_t io_config = {
+      .dc_gpio_num = LCD_DC_PIN,
+      .cs_gpio_num = LCD_CS_PIN,
+      .pclk_hz = LCD_PIXEL_CLOCK_HZ,
+      .lcd_cmd_bits = LCD_CMD_BITS,
+      .lcd_param_bits = LCD_PARAM_BITS,
+      .spi_mode = 0,
+      .trans_queue_depth = 10,
+      .on_color_trans_done = notify_lvgl_flush_ready,
+      .user_ctx = &disp_drv,
+  };
+  // Attach the LCD to the SPI bus
+  ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
+
+  esp_lcd_panel_handle_t panel_handle = NULL;
+  esp_lcd_panel_dev_config_t panel_config = {
+      .reset_gpio_num = LCD_RST_PIN,
+      .color_space = LCD_RGB_ENDIAN_RGB,
+      .bits_per_pixel = 16,
+  };
+  ESP_LOGI(_TAG, "install st7789 panel driver");
+  ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
+
+  // LCD reset and init
+  ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+  ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+  ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
+
+  // LCD on
+  ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+  
+  // Set backlight brightness
+  ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+  ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+  // ---
+
+  ESP_LOGI(_TAG, "initialize lvgl library");
+  lv_init();
+
+  // alloc draw buffers used by LVGL
+  // it's recommended to choose the size of the draw buffer(s) to be at least 1/10 LCD sized
+  lv_color_t *buf1 = heap_caps_malloc(LCD_H_RES * 20 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+  assert(buf1);
+  lv_color_t *buf2 = heap_caps_malloc(LCD_H_RES * 20 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+  assert(buf2);
+  // initialize LVGL draw buffers
+  lv_disp_draw_buf_init(&disp_buf, buf1, buf2, LCD_H_RES * 20);
+
+  ESP_LOGI(_TAG, "register lcd driver to lvgl");
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = LCD_H_RES;
+  disp_drv.ver_res = LCD_V_RES;
+  disp_drv.flush_cb = lvgl_flush_cb;
+  disp_drv.drv_update_cb = lvgl_port_update_callback;
+  disp_drv.draw_buf = &disp_buf;
+  disp_drv.user_data = panel_handle;
+  disp = lv_disp_drv_register(&disp_drv);
+
+  ESP_LOGI(_TAG, "install lvgl tick timer");
+  // Tick interface for LVGL (using esp_timer to generate periodic event)
+  const esp_timer_create_args_t lvgl_tick_timer_args = {
+      .callback = &increase_lvgl_tick,
+      .name = "lvgl_tick"
+  };
+  esp_timer_handle_t lvgl_tick_timer = NULL;
+  ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
+  ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_tick_timer, LVGL_TICK_PERIOD_MS * 1000));
+
+  // Create lvgl mutex
+  lvgl_mux = xSemaphoreCreateRecursiveMutex();
+  assert(lvgl_mux);
+}
--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -7,23 +7,20 @@ static const char *_TAG = "MAIN";
 #include "freertos/FreeRTOS.h"
 #include "freertos/ringbuf.h"
 #include "freertos/task.h"
-#include "nimble-nordic-uart.h"
 #include "driver/gpio.h"
 #include "esp_pm.h"
-#include "cJSON.h"
 #include "esp_sntp.h"
-#include "notifications.h"
-#include "lcd.h"
 #include "esp_sleep.h"
+#include "notifications.h"
+#include "gadgetbridge.h"
+#include "lcd.h"
+#include "lvgl.h"
 
 #define BUTTON_GPIO 9
 #define LED_L_GPIO 12
 #define LED_R_GPIO 13
 
 static QueueHandle_t gpio_event_queue = NULL;
-
-int bluetooth_connected = 0;
-int music_playing = 0;
 
 void update_lcd() {
   // Take lvgl mutex, blocking
@@ -37,6 +34,7 @@ void update_lcd() {
   static bool first_update = 1;
   static lv_obj_t *label_time, *label_status, *notification_container, *label_n_title, *label_n_body, *label_n_count;
   static lv_style_t style;
+  static lv_anim_t scroll_anim;
   if (first_update){
     lv_obj_set_style_bg_color(scr, lv_color_black(), LV_PART_MAIN);
     lv_obj_clear_flag(scr, LV_OBJ_FLAG_SCROLLABLE);
@@ -46,23 +44,30 @@ void update_lcd() {
     lv_style_set_text_font(&style, &lv_font_montserrat_18); // Default font
     lv_style_set_text_color(&style, lv_color_white());
 
+    // Set scrolling to not repeat
+    lv_anim_init(&scroll_anim);
+    lv_anim_set_delay(&scroll_anim, 500);
+    lv_anim_set_repeat_count(&scroll_anim, 0);  // This doesn't work :(
+    lv_anim_set_repeat_delay(&scroll_anim, 10000);
+    lv_style_set_anim(&style, &scroll_anim);
+
     label_time = lv_label_create(scr);
     lv_obj_align(label_time, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_add_style(label_time, &style, 0);
-    lv_obj_set_style_text_font(label_time, &lv_font_montserrat_48, 0); // Change font
+    lv_obj_add_style(label_time, &style, LV_PART_MAIN);
+    lv_obj_set_style_text_font(label_time, &lv_font_montserrat_48, LV_PART_MAIN); // Change font
 
     label_status = lv_label_create(scr);
     lv_obj_align(label_status, LV_ALIGN_TOP_RIGHT, -2, 2);
-    lv_obj_add_style(label_status, &style, 0);
-    lv_obj_set_style_text_color(label_status, lv_palette_main(LV_PALETTE_GREY), 0);
+    lv_obj_add_style(label_status, &style, LV_PART_MAIN);
+    lv_obj_set_style_text_color(label_status, lv_palette_main(LV_PALETTE_GREY), LV_PART_MAIN);
 
     notification_container = lv_obj_create(scr);
     lv_obj_align(notification_container, LV_ALIGN_BOTTOM_MID, 0, 54); // 54 is to start off screen
     lv_obj_set_width(notification_container, 240);
     lv_obj_set_height(notification_container, 52);
-    lv_obj_set_style_outline_width(notification_container, 2, 0);
-    lv_obj_set_style_outline_color(notification_container, lv_color_white(), 0);
-    lv_obj_set_style_pad_all(notification_container, 4, 0);
+    lv_obj_set_style_outline_width(notification_container, 2, LV_PART_MAIN);
+    lv_obj_set_style_outline_color(notification_container, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_style_pad_all(notification_container, 4, LV_PART_MAIN);
     lv_obj_set_style_bg_color(notification_container, lv_color_black(), LV_PART_MAIN);
     lv_obj_clear_flag(notification_container, LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_set_scrollbar_mode(notification_container, LV_SCROLLBAR_MODE_OFF);
@@ -71,21 +76,24 @@ void update_lcd() {
     lv_obj_align(label_n_title, LV_ALIGN_TOP_LEFT, 0, 0);
     lv_obj_set_width(label_n_title, 200);
     lv_label_set_long_mode(label_n_title, LV_LABEL_LONG_DOT);
-    lv_obj_add_style(label_n_title, &style, 0);
+    lv_obj_add_style(label_n_title, &style, LV_PART_MAIN);
     lv_obj_set_parent(label_n_title, notification_container);
 
     label_n_body = lv_label_create(scr);
     lv_obj_align(label_n_body, LV_ALIGN_BOTTOM_LEFT, 0, 0);
     lv_obj_set_width(label_n_body, 234);
-    lv_label_set_long_mode(label_n_body, LV_LABEL_LONG_DOT);
-    lv_obj_add_style(label_n_body, &style, 0);
+    lv_label_set_long_mode(label_n_body, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_obj_add_style(label_n_body, &style, LV_PART_MAIN);
     lv_obj_set_parent(label_n_body, notification_container);
+    lv_obj_set_style_anim(label_n_body, &scroll_anim, LV_PART_MAIN);
 
     label_n_count = lv_label_create(scr);
     lv_obj_align(label_n_count, LV_ALIGN_TOP_RIGHT, 0, 0);
-    lv_label_set_long_mode(label_n_count, LV_LABEL_LONG_DOT);
-    lv_obj_add_style(label_n_count, &style, 0);
-    lv_obj_set_style_text_color(label_n_count, lv_palette_main(LV_PALETTE_GREY), 0);
+    lv_obj_set_style_text_align(label_n_count, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
+    lv_obj_set_width(label_n_count, 30);
+    lv_label_set_long_mode(label_n_count, LV_LABEL_LONG_CLIP);
+    lv_obj_add_style(label_n_count, &style, LV_PART_MAIN);
+    lv_obj_set_style_text_color(label_n_count, lv_palette_main(LV_PALETTE_GREY), LV_PART_MAIN);
     lv_obj_set_parent(label_n_count, notification_container);
 
     first_update = 0;
@@ -97,10 +105,7 @@ void update_lcd() {
   localtime_r(&now, &timeinfo);
   lv_label_set_text_fmt(label_time, "%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min);
 
-  lv_label_set_text_fmt(label_status, "%s   %s", music_playing ? LV_SYMBOL_AUDIO : "", bluetooth_connected ? LV_SYMBOL_BLUETOOTH : "");
-
-  // lv_obj_t * sw = lv_event_get_target(e);
-  // lv_obj_t * label = lv_event_get_user_data(e);
+  lv_label_set_text_fmt(label_status, "%s  %s", music_playing ? LV_SYMBOL_AUDIO : "", bluetooth_connected ? LV_SYMBOL_BLUETOOTH : "");
 
   struct notification_t* n = latest_notification();
   if (n) {
@@ -119,6 +124,8 @@ void update_lcd() {
       }
   } else {
       lv_anim_t a;
+      lv_label_set_text(label_n_title, "");
+      lv_label_set_text(label_n_body, "");
       if (lv_obj_get_y(notification_container) == 188) {
         lv_anim_init(&a);
         lv_anim_set_var(&a, notification_container); // Object to animate
@@ -130,6 +137,7 @@ void update_lcd() {
       }
   }
   lv_label_set_text_fmt(label_n_count, "%d", notification_count());
+  lv_obj_align(label_n_count, LV_ALIGN_TOP_RIGHT, 0, 0);
 
   lvgl_reactivate();
 
@@ -156,137 +164,6 @@ void clock_task(void *parameter) {
 
   ESP_LOGE(_TAG, "gadgetbridge_task crashed");
   vTaskDelete(NULL);
-}
-
-// Send gadgetbridge message
-void gadgetbridge_send(const char *message) {
-  if (!bluetooth_connected) return;
-
-  ESP_LOGI(_TAG, "tx: %s", message);
-  _nordic_uart_send(message);
-  _nordic_uart_send(" \n");
-}
-
-// Process received gadgetbridge message
-void gadgetbridge_handle_receive(const char *message) {
-  ESP_LOGI(_TAG, "rx: %s", message);
-
-  char *json = malloc(strlen(message));
-  // If message is wrapped in GB(...) then do a nasty little hack to leave the json only
-  if (message[1] == 'G') {
-    for (int i = 0; i < strlen(message)-5; i++) json[i] = message[i+4];
-    json[strlen(message)-5] = '\0';
-  } else for (int i = 0; i < strlen(message); i++) json[i] = message[i];
-  ESP_LOGD(_TAG, "json: %s", json);
-
-  cJSON *root = cJSON_Parse(json);
-  if (cJSON_GetObjectItem(root, "t")) {
-    char *command = cJSON_GetObjectItem(root, "t")->valuestring;
-
-    if (strcmp(command, "notify") == 0) {
-      struct notification_t *n = new_notification();
-      
-      // Extract fields, making copy of strings beacuse cJSON has ownership of originals
-      if (cJSON_GetObjectItem(root, "id")) n->id = cJSON_GetObjectItem(root, "id")->valueint;
-      if (cJSON_GetObjectItem(root, "src")) n->src = strdup(cJSON_GetObjectItem(root, "src")->valuestring);
-      if (cJSON_GetObjectItem(root, "title")) n->title = strdup(cJSON_GetObjectItem(root, "title")->valuestring);
-      if (cJSON_GetObjectItem(root, "subject")) n->subject = strdup(cJSON_GetObjectItem(root, "subject")->valuestring);
-      if (cJSON_GetObjectItem(root, "body")) n->body = strdup(cJSON_GetObjectItem(root, "body")->valuestring);
-      if (cJSON_GetObjectItem(root, "sender")) n->sender = strdup(cJSON_GetObjectItem(root, "sender")->valuestring);
-      if (cJSON_GetObjectItem(root, "tel")) n->tel = strdup(cJSON_GetObjectItem(root, "tel")->valuestring);
-
-      if (n->id == 0) {
-        ESP_LOGW(_TAG, "no id so discarding");
-        delete_notification(n);
-      } else if (n->src == NULL) {
-        ESP_LOGW(_TAG, "no src so discarding as these are usually junk, id: %d", n->id);
-        delete_notification(n);
-      } else {
-        add_notification(n);
-        ESP_LOGI(_TAG, "%d active notifications", notification_count());
-        update_lcd();
-      }
-    } else if (strcmp(command, "notify-") == 0) {
-      int id = 0;
-      if (cJSON_GetObjectItem(root, "id")) id = cJSON_GetObjectItem(root, "id")->valueint;
-
-      // Clear notification if it exists
-      clear_notification(id);
-      ESP_LOGI(_TAG, "%d active notifications", notification_count());
-      update_lcd();
-    } else if (strcmp(command, "musicstate") == 0) {
-      char* state = "";
-      if (cJSON_GetObjectItem(root, "state")) state = cJSON_GetObjectItem(root, "state")->valuestring;
-      
-      if (strcmp(state, "play") == 0) music_playing = 1;
-      else if (strcmp(state, "pause") == 0) music_playing = 0;
-      ESP_LOGI(_TAG, "music state: \"%s\", %d", state, music_playing);
-      update_lcd();
-    } else {
-      ESP_LOGI(_TAG, "unsupported command");
-    }
-  } else {
-    // Not JSON, probably time set command
-    if (strstr(json,"setTime") != NULL) {
-      int utc = 0;
-      float gmt_offset = 0.0;
-      if (sscanf(json, "%*[^(]%*c%d%*[^(]%*c%f", &utc, &gmt_offset) == 2) {
-        char timezone_buf [8];
-        sprintf(timezone_buf, "GMT+%d", (int)gmt_offset);
-        setenv("TZ", timezone_buf, 1);
-        tzset();
-
-        struct timeval tv;
-        tv.tv_usec = 0;
-        tv.tv_sec = utc;
-        settimeofday(&tv, NULL);
-        
-        update_lcd();
-
-        ESP_LOGI(_TAG, "time set command, utc: %d, timezone: %s", utc, timezone_buf);
-      } else {
-        ESP_LOGE(_TAG, "failed to process time set command");;
-      }
-    } else {
-      ESP_LOGI(_TAG, "couldn't extract command");
-    }
-  }
-
-  cJSON_Delete(root);
-  free(json);
-}
-
-void gadgetbridge_task(void *parameter) {
-  while (1) {
-    if (nordic_uart_rx_buf_handle) {
-      // Get latest from FIFO buffer, blocking
-      size_t item_size;
-      const char *message = (char *)xRingbufferReceive(nordic_uart_rx_buf_handle, &item_size, portMAX_DELAY);
-
-      // If got a valid reference
-      if (message) {
-        gadgetbridge_handle_receive(message);
-        vRingbufferReturnItem(nordic_uart_rx_buf_handle, (void *)message); // Return reference to be removed from buffer
-      }
-    } else {
-      // Buffer not initialized so sleep
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-  }
-
-  ESP_LOGE(_TAG, "gadgetbridge_task crashed");
-  vTaskDelete(NULL);
-}
-
-void nordic_uart_callback (enum nordic_uart_callback_type callback_type) {
-  if (callback_type == NORDIC_UART_CONNECTED) {
-    bluetooth_connected = 1;
-  } else if (callback_type == NORDIC_UART_DISCONNECTED) {
-    bluetooth_connected = 0;
-    music_playing = 0;
-  }
-
-  update_lcd();
 }
 
 static void IRAM_ATTR gpio_isr_handler(void* arg) {
@@ -317,8 +194,7 @@ void app_main(void) {
   pm_config.light_sleep_enable = false; // Recommend disabling during development as can cause programming issues over USB
   if (esp_pm_configure((&pm_config)) != ESP_OK) ESP_LOGE(_TAG, "failed to configure light sleep");
 
-  _nordic_uart_start("Nordic UART", &nordic_uart_callback);
-  xTaskCreate(gadgetbridge_task, "gadgetbridge_task", 5000, NULL, 1, NULL);
+  gadgetbridge_init();
   xTaskCreate(clock_task, "clock_task", 5000, NULL, 1, NULL);
 
   gpio_event_queue = xQueueCreate(10, sizeof(uint32_t));
